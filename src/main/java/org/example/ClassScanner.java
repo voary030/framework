6e84d9901988dad5ef8d0d99025838d7241b8bcc
Voary <voary030@gmail.com>
2025-10-26 19:09:09 +0300

    sprint 2 bis

--- src/main/java/org/example/ClassScanner.java
+++ src/main/java/org/example/ClassScanner.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.File;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
@@ -28,13 +29,21 @@ public class ClassScanner {
     }
 
     public static void scan(String packageName) throws Exception {
+        // Obtenez les types d'annotations avec la sécurité de typage
+        @SuppressWarnings("unchecked")
+        Class<? extends Annotation> controllerAnnotation =
+                (Class<? extends Annotation>) Class.forName("org.example.annotation.Controller");
+        @SuppressWarnings("unchecked")
+        Class<? extends Annotation> urlAnnotation =
+                (Class<? extends Annotation>) Class.forName("org.example.annotation.Url");
+
         List<Class<?>> classes = getClasses(packageName);
         for (Class<?> c : classes) {
-            if (c.isAnnotationPresent((Class)Class.forName("test.Controller"))) {
+            if (c.isAnnotationPresent(controllerAnnotation)) {
                 System.out.println("🔹 Controller trouvé : " + c.getName());
                 for (Method m : c.getDeclaredMethods()) {
-                    if (m.isAnnotationPresent((Class)Class.forName("test.Url"))) {
-                        Object annotation = m.getAnnotation((Class)Class.forName("test.Url"));
+                    if (m.isAnnotationPresent(urlAnnotation)) {
+                        Annotation annotation = m.getAnnotation(urlAnnotation);
                         String urlValue = (String) annotation.getClass().getMethod("value").invoke(annotation);
                         System.out.println("   ↳ Méthode: " + m.getName() + "  URL: " + urlValue);
                     }
