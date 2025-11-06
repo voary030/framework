c26f581e30f195aacb630ea6b4efb5038cd459dc
Voary <voary030@gmail.com>
2025-11-06 03:43:30 +0300

    ok

--- src/main/java/org/example/annotation/Controller.java
+++ src/main/java/org/example/annotation/Controller.java
@@ -1,8 +1,11 @@
 package org.example.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Controller {
 }
