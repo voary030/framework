87fcd9ab1527b3d08a17b15f276ac8af04a5e6c2
Voary <voary030@gmail.com>
2025-11-06 23:18:59 +0300

    ok

--- src/main/java/org/example/annotation/Controller.java
+++ src/main/java/org/example/annotation/Controller.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.class)
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Controller {
 }
